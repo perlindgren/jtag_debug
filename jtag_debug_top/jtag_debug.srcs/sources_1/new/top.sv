0582bde40589ff22ac79bf47e4a65a3640755d3d
Per Lindgren <per.lindgren@ltu.se>
2024-04-04 17:56:39 +0200

    wip

--- jtag_debug_top/jtag_debug.srcs/sources_1/new/top.sv
+++ jtag_debug_top/jtag_debug.srcs/sources_1/new/top.sv
@@ -144,7 +144,11 @@ module top (
   dtmcs_t dtmcs_d, dtmcs_q;
 
 
-  // 
+  logic dmi_clear;  // Functional (warm) reset of the entire DMI
+
+  assign dmi_clear = DT_RESET || (DT_SEL && DT_UPDATE && dtmcs_q.dmihardreset);
+
+  // Debug Module Control and Status
   always_comb begin
     dtmcs_d = dtmcs_q;
     if (DT_CAPTURE) begin
@@ -167,5 +171,217 @@ module top (
       if (DT_SEL) dtmcs_d = {DT_TDI, 31'(dtmcs_q >> 1)};
     end
   end
+
+  always_ff @(posedge DT_TCK) begin
+    dtmcs_q <= dtmcs_d;
+  end
+
+  // Debug Module Interface
+
+  logic      dmi_select;
+  logic      dmi_tdo;
+
+  dmi_req_t  dmi_req;
+  logic      dmi_req_ready;
+  logic      dmi_req_valid;
+
+  dmi_resp_t dmi_resp;
+  logic      dmi_resp_valid;
+  logic      dmi_resp_ready;
+
+  state_e state_d, state_q;
+
+  dmi_t dr_d, dr_q;
+  logic [6:0] address_d, address_q;
+  logic [31:0] data_d, data_q;
+
+  dmi_t dmi;
+
+  assign dmi          = (dr_q);
+  assign dmi_req.addr = address_q;
+  assign dmi_req.data = data_q;
+
+
+
+
+
+
+
+  // logic [$bits(dmi_t)-1:0] dr_d, dr_q;
+  // logic [6:0] address_d, address_q;
+  // logic [31:0] data_d, data_q;
+
+  // dmi_t  dmi;
+  // assign dmi          = dmi_t'(dr_q);
+  // assign dmi_req.addr = address_q;
+  // assign dmi_req.data = data_q;
+  // assign dmi_req.op   = (state_q == Write) ? dm::DTM_WRITE : dm::DTM_READ;
+  // // We will always be ready to accept the data we requested.
+  // assign dmi_resp_ready = 1'b1;
+
+  // logic error_dmi_busy;
+  // logic error_dmi_op_failed;
+
+  // always_comb begin : p_fsm
+  //   error_dmi_busy = 1'b0;
+  //   error_dmi_op_failed = 1'b0;
+  //   // default assignments
+  //   state_d   = state_q;
+  //   address_d = address_q;
+  //   data_d    = data_q;
+  //   error_d   = error_q;
+
+  //   dmi_req_valid = 1'b0;
+
+  //   if (dmi_clear) begin
+  //     state_d   = Idle;
+  //     data_d    = '0;
+  //     error_d   = DMINoError;
+  //     address_d = '0;
+  //   end else begin
+  //     unique case (state_q)
+  //       Idle: begin
+  //         // make sure that no error is sticky
+  //         if (dmi_select && update && (error_q == DMINoError)) begin
+  //           // save address and value
+  //           address_d = dmi.address;
+  //           data_d = dmi.data;
+  //           if (dm::dtm_op_e'(dmi.op) == dm::DTM_READ) begin
+  //             state_d = Read;
+  //           end else if (dm::dtm_op_e'(dmi.op) == dm::DTM_WRITE) begin
+  //             state_d = Write;
+  //           end
+  //           // else this is a nop and we can stay here
+  //         end
+  //       end
+
+  //       Read: begin
+  //         dmi_req_valid = 1'b1;
+  //         if (dmi_req_ready) begin
+  //           state_d = WaitReadValid;
+  //         end
+  //       end
+
+  //       WaitReadValid: begin
+  //         // load data into register and shift out
+  //         if (dmi_resp_valid) begin
+  //           unique case (dmi_resp.resp)
+  //             dm::DTM_SUCCESS: begin
+  //               data_d = dmi_resp.data;
+  //             end
+  //             dm::DTM_ERR: begin
+  //               data_d = 32'hDEAD_BEEF;
+  //               error_dmi_op_failed = 1'b1;
+  //             end
+  //             dm::DTM_BUSY: begin
+  //               data_d = 32'hB051_B051;
+  //               error_dmi_busy = 1'b1;
+  //             end
+  //             default: begin
+  //               data_d = 32'hBAAD_C0DE;
+  //             end
+  //           endcase
+  //           state_d = Idle;
+  //         end
+  //       end
+
+  //       Write: begin
+  //         dmi_req_valid = 1'b1;
+  //         // request sent, wait for response before going back to idle
+  //         if (dmi_req_ready) begin
+  //           state_d = WaitWriteValid;
+  //         end
+  //       end
+
+  //       WaitWriteValid: begin
+  //         // got a valid answer go back to idle
+  //         if (dmi_resp_valid) begin
+  //           unique case (dmi_resp.resp)
+  //             dm::DTM_ERR: error_dmi_op_failed = 1'b1;
+  //             dm::DTM_BUSY: error_dmi_busy = 1'b1;
+  //             default: ;
+  //           endcase
+  //           state_d = Idle;
+  //         end
+  //       end
+
+  //       default: begin
+  //         // just wait for idle here
+  //         if (dmi_resp_valid) begin
+  //           state_d = Idle;
+  //         end
+  //       end
+  //     endcase
+
+  //     // update means we got another request but we didn't finish
+  //     // the one in progress, this state is sticky
+  //     if (update && state_q != Idle) begin
+  //       error_dmi_busy = 1'b1;
+  //     end
+
+  //     // if capture goes high while we are in the read state
+  //     // or in the corresponding wait state we are not giving back a valid word
+  //     // -> throw an error
+  //     if (capture && state_q inside {Read, WaitReadValid}) begin
+  //       error_dmi_busy = 1'b1;
+  //     end
+
+  //     if (error_dmi_busy && error_q == DMINoError) begin
+  //       error_d = DMIBusy;
+  //     end
+
+  //     if (error_dmi_op_failed && error_q == DMINoError) begin
+  //       error_d = DMIOPFailed;
+  //     end
+
+  //     // clear sticky error flag
+  //     if (update && dtmcs_q.dmireset && dtmcs_select) begin
+  //       error_d = DMINoError;
+  //     end
+  //   end
+  // end
+
+  // // shift register
+  // assign dmi_tdo = dr_q[0];
+
+  // always_comb begin : p_shift
+  //   dr_d    = dr_q;
+  //   if (dmi_clear) begin
+  //     dr_d = '0;
+  //   end else begin
+  //     if (capture) begin
+  //       if (dmi_select) begin
+  //         if (error_q == DMINoError && !error_dmi_busy) begin
+  //           dr_d = {address_q, data_q, DMINoError};
+  //           // DMI was busy, report an error
+  //         end else if (error_q == DMIBusy || error_dmi_busy) begin
+  //           dr_d = {address_q, data_q, DMIBusy};
+  //         end
+  //       end
+  //     end
+
+  //     if (shift) begin
+  //       if (dmi_select) begin
+  //         dr_d = {tdi, dr_q[$bits(dr_q)-1:1]};
+  //       end
+  //     end
+  //   end
+  // end
+
+  // always_ff @(posedge tck or negedge trst_ni) begin
+  //   if (!trst_ni) begin
+  //     dr_q      <= '0;
+  //     state_q   <= Idle;
+  //     address_q <= '0;
+  //     data_q    <= '0;
+  //     error_q   <= DMINoError;
+  //   end else begin
+  //     dr_q      <= dr_d;
+  //     state_q   <= state_d;
+  //     address_q <= address_d;
+  //     data_q    <= data_d;
+  //     error_q   <= error_d;
+  //   end
+  // end
 
 endmodule
